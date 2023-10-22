7644c94f265d878c1d5d71731f5dc15f2b2dbcad
Arun Hariharan <arunhariharan21@gmail.com>
2023-10-22 14:36:36 -0400

    Integrating Express with Socket.io innit

--- server.js
+++ server.js
@@ -1,39 +1,18 @@
-const { read } = require('fs');
+const api = require('./api');
 
-const server = require('http').createServer();
+const server = require('http').createServer(api);
 const io = require('socket.io')(server, {
     cors: {
       origin: '*',
       methods: ['GET', 'POST']
     }
 });
+
+const sockets = require('./sockets');
+
 const PORT = 3000;
 
 server.listen(PORT);
 console.log(`Listening on port ${PORT}...`);
 
-let readyPlayerCount = 0; 
-
-io.on('connection', (socket) => {
-  console.log('a user connected', socket.id);
-  
-  socket.on('ready', () => {
-    console.log('Player ready', socket.id);
-    readyPlayerCount++; 
-    if(readyPlayerCount % 2 === 0){
-      io.emit('startGame', socket.id); //Sets the second player ready as referee 
-    }
-  });
-
-  socket.on('paddleMove', (paddleData) => {
-    socket.broadcast.emit('paddleMove', paddleData);
-  });
-
-  socket.on('ballMove', (ballData) => {
-    socket.broadcast.emit('ballMove', ballData);
-  });
-
-  socket.on('disconnect', (reason) => {
-    console.log(`Client ${socket.id} disconnected: ${reason}`);
-  }); 
-});
+sockets.listen(io);
